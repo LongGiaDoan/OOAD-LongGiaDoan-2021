627716a91b7557da7b3ef3708898b49344d2ee61
LongGiaDoan <56022256+LongGiaDoan@users.noreply.github.com>
2021-03-14 21:47:34 +0100

    notepad deel 2

--- SlnLes04WpfLayout/WpfNotepad/MainWindow.xaml.cs
+++ SlnLes04WpfLayout/WpfNotepad/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,13 @@ namespace WpfNotepad
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string chosenFileName;
+        private string folderpath;
+        private string content;
         public MainWindow()
         {
             InitializeComponent();
+            folderpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         }
 
         private void mnuAbout_Click(object sender, RoutedEventArgs e)
@@ -30,6 +36,121 @@ namespace WpfNotepad
             AboutWindow win = new AboutWindow();
             win.Show();
 
+        }
+
+        private void btnCut_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetDataObject(txtTekst.SelectedText);
+            txtTekst.SelectedText = "";
+        }
+
+        private void btnPaste_Click(object sender, RoutedEventArgs e)
+        {
+            IDataObject iData = Clipboard.GetDataObject();
+            txtTekst.Paste();
+            Clipboard.Clear();
+
+        }
+
+        private void btnCopy_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetDataObject(txtTekst.SelectedText);
+        }
+
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int aantal;
+            aantal = txtTekst.Text.Count(char.IsLetterOrDigit);
+            lblAantalWoorden.Content = $"#chars: {aantal}";
+
+
+        }
+
+        private void mnuNew_Click(object sender, RoutedEventArgs e)
+        {
+            txtTekst.Text = "";
+        }
+
+        private void mnuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = folderpath;
+            dialog.Filter = "Tekstbestanden|*.TXT;*.TEXT";
+            string chosenFileName;
+            bool? dialogResult = dialog.ShowDialog();
+            if (dialogResult != true) return;
+            // user picked a file and pressed OK
+            chosenFileName = dialog.FileName;
+            FileInfo fi = new FileInfo(chosenFileName);
+            tbiNaamDoc.Header = fi.Name;
+            // bestand schrijven
+
+
+            string filePath = System.IO.Path.Combine(folderpath, chosenFileName);
+            // open stream and start reading
+            using (StreamReader reader = File.OpenText(filePath))
+            {
+                content = reader.ReadToEnd();
+                txtTekst.Text = content;
+
+
+            }
+
+        }
+
+        private void mnuSaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.InitialDirectory = folderpath;
+            dialog.Filter = "Tekstbestanden|*.TXT;*.TEXT";
+            if (dialog.ShowDialog() == true)
+            {
+                using (StreamWriter writer = File.CreateText(dialog.FileName))
+                {
+
+                    content = txtTekst.Text;
+                    writer.WriteLine(content);
+
+                }
+            }
+        }
+
+        private void mnuSave_Click(object sender, RoutedEventArgs e)
+        {
+            FileInfo fi = new FileInfo(chosenFileName);
+            if (fi.Exists == false)
+            {
+                mnuSaveAs_Click(sender, e);
+            }
+            else
+            {
+                string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string filePath = System.IO.Path.Combine(folderPath, (string)tbiNaamDoc.Content);
+                // write all text at once
+                File.WriteAllText(filePath, content);
+            }
+
+
+        }
+
+        private void MenuItem_SubmenuOpened(object sender, RoutedEventArgs e)
+        {
+            if (tbiNaamDoc.Content != null)
+            {
+                mnuSave.IsEnabled = true;
+                mnuSaveAs.IsEnabled = true;
+            }
+        }
+
+        private void mnuEdit_SubmenuOpened(object sender, RoutedEventArgs e)
+        {
+            if (Clipboard.ContainsText() == true)
+            {
+                btnPaste.IsEnabled = true;
+            }
+            else btnPaste.IsEnabled = false;
+            
         }
     }
 }
